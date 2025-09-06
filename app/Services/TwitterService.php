b4662678ecb0c0afad67552ddbc2dcebe595dce8
priyasrivastava3111 <erpriyasri@gmail.com>
2025-09-06 09:14:59 +0530

    Updated TwitterService file

--- app/Services/TwitterService.php
+++ app/Services/TwitterService.php
@@ -13,15 +13,10 @@ class TwitterService
 
     public function __construct()
  {
-        // $this->apiKey = env( 'TWITTER_API_KEY' );
-        // $this->apiSecret = env( 'TWITTER_API_SECRET' );
-        // $this->accessToken = env( 'TWITTER_ACCESS_TOKEN' );
-        // $this->accessSecret = env( 'TWITTER_ACCESS_SECRET' );
-        $this->apiKey = 'X1wViljXufXkbYt1BcsPu08ZX';
-        $this->apiSecret = 'ZqhyqUFQ3jiqNvi752l1hXtWdvciSN69ekhfYANQMgFwg3c2WS';
-        $this->accessToken = '1962794287227379717-SnuF5HNp1tec1hCTrofiRngjRk68Ea';
-        $this->accessSecret = 'P3dmO7YnXpTHTUtRAPmKRs2Dlf2UkjItVnJzVWazvS3I7';
-        
+        $this->apiKey = env( 'TWITTER_API_KEY' );
+        $this->apiSecret = env( 'TWITTER_API_SECRET' );
+        $this->accessToken = env( 'TWITTER_ACCESS_TOKEN' );
+        $this->accessSecret = env( 'TWITTER_ACCESS_SECRET' ); 
 
     }
 
